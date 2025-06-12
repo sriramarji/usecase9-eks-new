7aa790d1fbe4c5bf6ba0269c868ef762329b09b4
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-12 23:09:44 +0530

    add issue fix

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -37,7 +37,7 @@ data "aws_partition" "current" {}
 resource "aws_iam_openid_connect_provider" "oidc_provider" {
   client_id_list  = ["sts.${data.aws_partition.current.dns_suffix}"]
   thumbprint_list = [var.eks_oidc_root_ca_thumbprint]
-  url             = aws_eks_cluster.eks.certificate_authority[0].data
+  url             = aws_eks_cluster.eks.identity[0].oidc[0].issuer
 
   tags = {
       Name = "${var.name}-eks-irsa"
