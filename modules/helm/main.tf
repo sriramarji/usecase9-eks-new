43281b3e01d8fe7dc6e1a065227cb1fc3443c43b
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-13 00:01:00 +0530

    outputs

--- modules/helm/main.tf
+++ modules/helm/main.tf
@@ -105,7 +105,13 @@ data "kubernetes_service" "prometheus" {
 }
 
 
+output "grafana_url" {
+  value = data.kubernetes_service.grafana.status[0].load_balancer[0].ingress[0].hostname
+}
 
+output "prometheus_url" {
+  value = data.kubernetes_service.prometheus.status[0].load_balancer[0].ingress[0].hostname
+}
 
 
 # Resource: Kubernetes Ingress Class
--- modules/helm/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "grafana_url" {
-  value = data.kubernetes_service.grafana.status[0].load_balancer[0].ingress[0].hostname
-}
-
-output "prometheus_url" {
-  value = data.kubernetes_service.prometheus.status[0].load_balancer[0].ingress[0].hostname
-}
