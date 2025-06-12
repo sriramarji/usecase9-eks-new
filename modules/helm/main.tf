597c5a8a8198a0f3e682deefcec180cd99703b27
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-12 23:33:40 +0530

    command the ingressclass

--- modules/helm/main.tf
+++ modules/helm/main.tf
@@ -64,15 +64,15 @@ resource "helm_release" "loadbalancer_controller" {
 }
 
 # Resource: Kubernetes Ingress Class
-resource "kubernetes_ingress_class_v1" "ingress_class_default" {
-  depends_on = [helm_release.loadbalancer_controller]
-  metadata {
-    name = "my-aws-ingress-class"
-    annotations = {
-      "ingressclass.kubernetes.io/is-default-class" = "true"
-    }
-  }  
-  spec {
-    controller = "ingress.k8s.aws/alb"
-  }
-}
+#resource "kubernetes_ingress_class_v1" "ingress_class_default" {
+#  depends_on = [helm_release.loadbalancer_controller]
+#  metadata {
+#    name = "my-aws-ingress-class"
+#    annotations = {
+#      "ingressclass.kubernetes.io/is-default-class" = "true"
+#    }
+#  }  
+#  spec {
+#    controller = "ingress.k8s.aws/alb"
+#  }
+#}
