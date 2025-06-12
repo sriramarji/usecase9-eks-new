48ec0d627b32105b159f8d5c260d4cea9e26aad2
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-13 01:41:20 +0530

    add

--- modules/helm/main.tf
+++ modules/helm/main.tf
@@ -3,6 +3,13 @@ data "aws_eks_cluster_auth" "cluster" {
   name = var.cluster_id
 }
 
+# Kubernetes Provider
+provider "kubernetes" {
+  host                   = var.cluster_endpoint
+  cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
 # HELM Provider
 provider "helm" {
   kubernetes {
