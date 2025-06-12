55bf219b4068b2cc8a4dd4b8edfe686ba015f42f
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-13 01:19:38 +0530

    namespace

--- modules/helm/main.tf
+++ modules/helm/main.tf
@@ -63,14 +63,11 @@ resource "helm_release" "loadbalancer_controller" {
     
 }
 
-resource "kubernetes_namespace" "monitoring" {
-  metadata {
-    name = "monitoring"
-  }
-}
 
 resource "helm_release" "prometheus" {
   name       = "prometheus"
+  namespace  = "monitoring"
+  create_namespace = true
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus"
@@ -81,7 +78,7 @@ resource "helm_release" "prometheus" {
 
 resource "helm_release" "grafana" {
   name       = "grafana"
-  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+  namespace  = "monitoring"
   repository = "https://grafana.github.io/helm-charts"
   chart      = "grafana"
   version    = "7.3.10" # Choose latest compatible
