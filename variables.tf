256fa3b3b4a8798adcdd0929c527f356c52db2c9
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-13 12:25:50 +0530

    add instruction oidc

--- variables.tf
+++ variables.tf
@@ -22,3 +22,7 @@ variable "eks_oidc_root_ca_thumbprint" {
   description = "Thumbprint of Root CA for EKS OIDC, Valid until 2037"
   default     = "9e99a48a9960b14926bb7f3b02e22da2b0ab7280"
 }
+
+#OIDC Provider: Amazon EKS uses OIDC for IAM identity mapping. This allows you to map AWS IAM roles to Kubernetes service accounts, enabling fine-grained control over permissions in your Kubernetes clusters.
+
+#Root CA Thumbprint: The thumbprint is used to verify the OIDC identity provider (IdP). The thumbprint allows you to ensure that you're connecting to the correct OIDC provider endpoint, preventing Man-In-The-Middle (MITM) attacks.
